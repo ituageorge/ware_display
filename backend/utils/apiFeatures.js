c34ef8aa6b9729e5281200bb1ce98ab280f3b8b4
ituaosagie100@gmail.com <ituaosagie100@gmail.com>
2023-06-28 08:51:05 +0100

    added search, pagination and filter

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -21,6 +21,34 @@ class APIFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    filter() {
+        const queryCopy = { ...this.queryStr }
+
+           //removing fields from the query
+           const removeFields = ['keyword', 'limit', 'page' ]
+           removeFields.forEach(el => delete queryCopy[el]);
+            
+           //Advance filter for price, ratings etc
+           let queryStr = JSON.stringify(queryCopy);
+           queryStr = queryStr.replace(/\b(gt| gte|lte|lte)\b/g, match => `$${match}` )
+
+           this.query = this.query.find(JSON.parse(queryStr));
+           return this;
+
+    }
+
+    pagination(resPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+    
+        this.query = this.query.limit(resPerPage).skip(skip);
+        return this;
+
+
+        
+    }
 }
 
 module.exports = APIFeatures
