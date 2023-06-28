37648c633acfec2ef5c0d4af0c1028f6ff52d977
ituaosagie100@gmail.com <ituaosagie100@gmail.com>
2023-06-28 17:58:07 +0100

    login authentication

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -24,3 +24,32 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     })
 })
 
+//Login user => /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const {email, password} = req.body;
+
+    //check if email and password is entered by user
+    if(!email || !password){
+        return next(new ErrorHandler('Please provide an Email & Password!', 400))
+    }
+
+    //Finding the user in database
+    let foundUser = await User.findOne({email}).select('+password');
+
+    if(!foundUser) {
+    return next(new ErrorHandler('Invalid Email or Password'))
+    }
+
+    //checks if password is correct or not
+    const isPasswordMatched = await foundUser.comparePassword(password);
+
+    if(!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid Credentials", 401));
+    }
+
+    const token = foundUser.getJwtToken();
+    res.status(200).json({
+        success: true,
+        token
+    });
+})
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router();
 
 const {registerUser} = require('../controllers/usersController');
+const {loginUser } = require('../controllers/usersController');
+
+router.route('/login').post(loginUser);
 router.route('/register').post(registerUser);
 
 module.exports=router;
